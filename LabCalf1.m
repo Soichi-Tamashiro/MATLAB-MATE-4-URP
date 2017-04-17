5663f89a75ed0582fd7fc3832b23f80be7d41b18
Soichi Tamashiro <wyvernys@live.com>
2017-04-17 11:31:40 -0500

    Add files via upload

--- LabCalf1.m
+++ LabCalf1.m
@@ -9,10 +9,24 @@
 %
 % 1.-
 %
-%   i)
-
+    z=cplxgrid(30);
+%   i) g = abs(z^2 + 2*z + 3)^3;
+    g = abs(z.^2 + 2*z + 3).^3;
+    figure('name','g = abs(z^2 + 2*z + 3)^3');
+    cplxmap(z,g)
 %   ii)
+    h = floor(z+5).^4;
+    figure('name','h = floor(z+5).^4');
+    cplxmap(z,h)
 %   iii)
+    T = (z.^3).^(1/3);
+    figure('name','T = (z.^3).^(1/3)');
+    cplxmap(z,T)
+%   iv)
+    G =exp(z.^5 + z +1);
+    figure('name','G =exp(z.^5 + z +1');
+    cplxmap(z,G)
+    
 % 2.-
 %   F = (sin(z)+sin(z)^3-sin(z)*cos(z))/(sin(z)*z^2);
     syms z;
