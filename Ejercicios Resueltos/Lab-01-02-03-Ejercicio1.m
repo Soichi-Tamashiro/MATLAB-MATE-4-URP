4e4bcf651fb40cdfe87153bc928072ab0a683c97
Soichi-Tamashiro <wyvernys@live.com>
2017-04-27 22:32:03 -0500

    Ordenado de labs de mate 4

---
+++ Ejercicios Resueltos/Lab-01-02-03-Ejercicio1.m
@@ -0,0 +1,104 @@
+% Clear Command Window
+    clc
+% Close all Open Figures( graphs )
+    close all
+% Clear Workspace
+    clear
+% Asignatura : Matemática IV
+%
+% Gráficas, Límites y continuidad en C - Derivación y Aplicación
+% 1.-
+% Escribe como texto y usando Software Matlab graficar las siguientes
+% funciones en el plano de Argand ż Qué nombre genérico tiene estas
+% superficies.
+% 
+% i)    F(z) = z^3
+    z = cplxgrid(30);
+    f = z.^3;
+    figure('name','F(z) = z^3'); % Change the name of the Figure
+    cplxmap(z,f)
+% ii)   g(z) = 1/(4*z^4)
+    z = cplxgrid(30);
+    g = 1./(4*z.^2);
+    figure('name','g(z) = 1/(4*z^4)');
+    cplxmap(z,g)
+% iii)  h(z) = z^3 + 4*z + 6
+    z = cplxgrid(30);
+    h = z.^3 + 4*z + 6;
+    figure('name','h(z) = z^3 + 4*z + 6')
+    cplxmap(z,h)
+% iv)   G(z) = abs(24/z)
+    z = cplxgrid(30);
+    G = abs(24./z);
+    figure('name','G(z) = abs(24/z)')
+    cplxmap(z,G)
+% v)    H(z)= (z^3-27)^(1/3)
+    z = cplxgrid(30);
+    H = (z.^3-27).^(1/3);
+    figure('name','H(z)= (z^3-27)^(1/3)')
+    cplxmap(z,H)
+% vi)   F(z) = (z+9)^(1/2)
+    z = cplxgrid(20);
+    F = (z+9).^(1/2);
+    figure('name','F(z) = (z+9)^(1/2)')
+    cplxmap(z,F)
+% vii)  H(z) = floor(z)^2
+    z = cplxgrid(30);
+    H = floor(z).^2;
+    figure('name','H(z) = floor(z)^2')
+    cplxmap(z,H)
+% viii) T(z) = abs(z)
+    z = cplxgrid(30);
+    T = abs(z);
+    figure('name','T(z) = abs(z)')
+    cplxmap(z,T)
+% ix)   S(z) = abs(z)^2
+    z = cplxgrid(30);
+    S = abs(z).^2;
+    figure('name','S(z) = abs(z)^2')
+    cplxmap(z,S)
+% x)    g(z) = z^4
+    z = cplxgrid(30);
+    g = g.^2;
+    figure('name','g(z) = z^4')
+    cplxmap(z,g)
+% xi)   H(z) = cos(z)^2
+    z = cplxgrid(30);
+    H = cos(z).^2;
+    figure('name','H(z) = cos(z)^2')
+    cplxmap(z,g)
+% xii)  h(z) = sin(z)^3
+    z = cplxgrid(30);
+    h = sin(z).^3;
+    figure('name','h(z) = sin(z)^3')
+    cplxmap(z,h)
+% xiii) m(z) = abs(z)^3
+    z = cplxgrid(30);
+    m = abs(z).^3;
+    figure('name','m(z) = abs(z)^3')
+    cplxmap(z,m)
+% xiv)  n(z) = z^(1/3)
+    z = cplxgrid(30);
+    n = z.^(1/3);
+    figure('name','n(z) = z^(1/3)')
+    cplxmap(z,n)
+% xv)   p(z) = abs(z)^3 + abs(z) + 6
+    z = cplxgrid(30);
+    p = abs(z).^3 + abs(z) +6;
+    figure('name','p(z) = abs(z)^3 + abs(z) + 6')
+    cplxmap(z,p)
+% xvi)  M(z) = log(z^2)
+    z = cplxgrid(30);
+    M = log(z.^2);
+    figure('name','M(z) = log(z^2)')
+    cplxmap(z,M)
+% xvii) N(z) = abs(z^2)^(1/3)
+    z = cplxgrid(30);
+    N = abs(z.^2).^(1./3);
+    figure('name','N(z) = abs(z^2)^(1/3)')
+    cplxmap(z,N)
+% xviii)P(z) = abs(z)^(1/2) + z^2 + 5*z +3
+    z = cplxgrid(30);
+    P = abs(z).^(1/2) + z.^2 +5*z +3;
+    figure('name','P(z) = abs(z)^(1/2) + z^2 + 5*z +3')
+    cplxmap(z,P)
---
+++ Laboratorios Clase/Lab01mate4.m
@@ -0,0 +1,72 @@
+clc
+% Usando el Software MATLAB, graficar las siguientes Funciones(superficies)
+% Graficar usando cplxmap(variable,funcion)
+% i
+z=cplxgrid(30);
+f=z.^3;
+cplxmap(z,f)
+% ---------------------
+% iii
+h=z.^3+4*z+6;
+cplxmap(z,h)
+%----------------------
+% v
+H=(z.^3-27).^(1/3);
+cplxmap(z,H)
+%----------------------
+% vii ( maximo entero => floor
+G=floor(z.^2);
+cplxmap(z,G)
+%----------------------
+% viii
+T=abs(z);
+cplxmap(z,T)
+%----------------------
+% ix
+S=(abs(z)).^2;
+cplxmap(z,S)
+%----------------------
+% xii
+h=sin(z).^3;
+cplxmap(z,h)
+%----------------------
+% xvi Warning: Log of zero.
+M=log(z.^2);
+cplxmap(z,M)
+% El nombre generico de esta superficie es :
+% "Superficies de Riemann"
+%--------------------------------------------------------------------------
+
+% 2) Evaluar los siguientes Limites con el Software MATLAB
+% Evaluar usando L = limit(funcion,variable,punto de evaluacion de limite)
+% a)
+syms z
+f=(z^15+i)/(z^7+i);
+L=limit(f,z,i)
+%----------------------
+% c)
+g=(cos(z))^(1/z^2);
+L1=limit(g,z,0)
+%----------------------
+% d)
+n=(sin(z)/z)^(1/z^2);
+L2=limit(n,z,0)
+%----------------------
+% k)
+n2=(tan(z)^12-1)/(z-pi/4);
+L3=limit(n2,z,pi/4)
+%----------------------
+% l)
+s=((cos(z))^(1/5)-cos(z)^(1/9))/(sin(z))^2;
+L4=limit(s,z,0)
+%--------------------------------------------------------------------------
+
+% 3) Evalue los siguientes Limites
+% i
+T=(z^5+z-34*i)/(z-2*i);
+L5=limit(T,z,2*i)
+% iv
+F=(z-sin(z))/(z*sin(z))^(3/2);
+L6=limit(F,z,0)
+F1=((z-sin(z))/(z^3))*(sin(z)/z)^(-3/2);
+L6=limit(F1,z,0)
