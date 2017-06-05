e7a8c56b70a520ecacd4c7e58f09319f31a234ec
Soichi Tamashiro <wyvernys@live.com>
2017-06-05 11:19:42 -0500

    Add files via upload
    Pregunta 3

---
+++ Laboratorios Calificados/LabCalf3.m
@@ -0,0 +1,35 @@
+% Clear Command Window
+    clc
+% Close all Open Figures( graphs )
+    close all
+% Clear Workspace
+    clear
+% Asignatura : Matemática IV
+%
+% 1.-
+%   i)  I=int(x^2-iy^2)dz
+%       y=2*x^2
+%       P0(1,1) Q0(2,8)
+%--------------------------------------------------------
+% 2.-
+%   i)  I=int(x^2*ydx+(y^3-x*y^2)dy)
+%       |z||=5
+%
+%--------------------------------------------------------
+% 3.-
+%
+%   i)   F(z)=(60*z^2-820*z+2845)/((2*z-13)^3*(9-z)^3)
+%        z=1,3,4,6 ordenes 5,6,7,8
+syms z;
+F = (60*z^2-820*z+2845)/((2*z-13)^3*(9-z)^3);
+pretty(F)
+T1=taylor(F,z,6,1);
+pretty(T1)
+T2=taylor(F,z,7,2);
+pretty(T2)
+T3=taylor(F,z,8,3);
+pretty(T3)
+T4=taylor(F,z,9,4);
+pretty(T4)
+
+
