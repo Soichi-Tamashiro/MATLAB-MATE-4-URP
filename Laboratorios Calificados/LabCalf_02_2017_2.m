3bab75dc6a2a8e9ba8a611d254c7f2966e96ec05
Soichi Tamashiro <wyvernys@live.com>
2017-10-02 14:10:23 -0500

    lab 2

---
+++ Laboratorios Calificados/LabCalf_02_2017_2.m
@@ -0,0 +1,11 @@
+% Clear Command Window
+    clc
+% Close all Open Figures( graphs )
+    close all
+% Clear Workspace
+    clear
+% Asignatura : Matemática IV
+% 1.-
+syms x y;
+V=84*x^2*y-18*y^3+4*x^2-4*y^2-20*y+200
+pretty(V)
