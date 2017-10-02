9975bf06d38d3666e6dcd9e8b362226b397defbe
Soichi Tamashiro <wyvernys@live.com>
2017-10-02 14:19:06 -0500

    lab2

--- Laboratorios Calificados/LabCalf_02_2017_2.m
+++ Laboratorios Calificados/LabCalf_02_2017_2.m
@@ -6,6 +6,17 @@
     clear
 % Asignatura : Matemática IV
 % 1.-
-syms x y;
+syms x y cte1y dcte1y;
 V=84*x^2*y-18*y^3+4*x^2-4*y^2-20*y+200
 pretty(V)
+Vy= diff(V,y)
+pretty(Vy)
+Ux=Vy
+pretty(Ux)
+U=int(Ux,x)
+pretty(U)
+U = U+cte1y
+pretty(U)
+Uy=diff(U,y)
+pretty(Uy)
+Uy=Uy+dcte1y
