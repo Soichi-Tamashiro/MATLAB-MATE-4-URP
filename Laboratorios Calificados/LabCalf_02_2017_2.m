dd8c787c08bfea5205817d9898879f5d96d295b1
Soichi Tamashiro <wyvernys@live.com>
2017-10-02 15:21:35 -0500

    lab2

--- Laboratorios Calificados/LabCalf_02_2017_2.m
+++ Laboratorios Calificados/LabCalf_02_2017_2.m
@@ -6,7 +6,7 @@
     clear
 % Asignatura : Matemática IV
 % 1.-
-syms x y cte1y dcte1y;
+syms x y cte1y dcte1y C2 z C3 C4;
 V=84*x^2*y-18*y^3+4*x^2-4*y^2-20*y+200
 pretty(V)
 % Derivo respecto a y
@@ -28,3 +28,29 @@ Uy=Uy+dcte1y
 Vx=-(Uy)
 Vx=diff(V,x)
 pretty(Vx)
+dcte1y = -60*y*x
+U = U - cte1y
+%cte1y = int(dcte1y,y)+C2
+U = U + int(dcte1y,y)+C2
+Fii=U+i*V
+pretty(Fii)
+Fii=subs(Fii,x,z)
+pretty(Fii)
+Fii=subs(Fii,y,0)
+pretty(Fii)
+Fi= int(Fii,z)+C3
+pretty(Fi)
+F= int(Fi,z)+C4
+pretty(F)
+FiI= subs(Fi,z,i)
+pretty(FiI)
+F1= subs(F,z,1)
+pretty(F1)
+F1=subs(F1,C2,0)
+F1=subs(F1,C3,545/3)
+F1=-F1+C4+301*i/3
+pretty(F1)
+F=subs(F,C2,0)
+F=subs(F,C3,545/3)
+F=subs(F,C4,-2696/15)
+pretty(F)
