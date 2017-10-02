00f78916eee1dcedcf58a1879a145b35fead7cc1
Soichi Tamashiro <wyvernys@live.com>
2017-10-02 14:27:33 -0500

    lab2

--- Laboratorios Calificados/LabCalf_02_2017_2.m
+++ Laboratorios Calificados/LabCalf_02_2017_2.m
@@ -11,8 +11,10 @@ V=84*x^2*y-18*y^3+4*x^2-4*y^2-20*y+200
 pretty(V)
 Vy= diff(V,y)
 pretty(Vy)
+% Por cauchy riemann
 Ux=Vy
 pretty(Ux)
+% Integro respecto a x
 U=int(Ux,x)
 pretty(U)
 U = U+cte1y
@@ -20,3 +22,7 @@ pretty(U)
 Uy=diff(U,y)
 pretty(Uy)
 Uy=Uy+dcte1y
+% Por cauchy riemann
+Vx=-(Uy)
+Vx=diff(V,x)
+pretty(Vx)
